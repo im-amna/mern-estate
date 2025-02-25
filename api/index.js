7aa6abcc252a139f292054d228c21f95c9da46a8
im-amna <amna160304@gmail.com>
2025-02-25 20:44:46 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
+import { error } from "console";
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to Mongodb");
+  })
+  .catch((error) => {
+    console.log("error" + error);
+  });
 
 const app = express();
 app.listen(3000, () => {
