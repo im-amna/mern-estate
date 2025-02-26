f26b20841362073712a29c066b5c63d8820bb293
im-amna <amna160304@gmail.com>
2025-02-26 15:37:34 +0500

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
-import { error } from "console";
 import express from "express";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 import mongoose from "mongoose";
 dotenv.config();
 mongoose
@@ -14,8 +14,10 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
