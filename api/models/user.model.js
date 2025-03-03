aebd5c968c21e049c64ae684399f45f2ce8c93e3
im-amna <amna160304@gmail.com>
2025-03-03 20:58:48 +0500

    update the header and make the profile page private

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    avator: {
+    avatar: {
       type: String,
       default:
         "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
@@ -26,4 +26,5 @@ const userSchema = new mongoose.Schema(
 );
 
 const User = mongoose.model("User", userSchema);
+
 export default User;
