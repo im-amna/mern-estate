92ae8a0dc5e5cdd9967c93e165ddeff51df87c07
im-amna <amna160304@gmail.com>
2025-03-04 22:53:15 +0500

    complete image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-18ad0.firebaseapp.com",
-  projectId: "mern-estate-18ad0",
-  storageBucket: "mern-estate-18ad0.firebasestorage.app",
-  messagingSenderId: "385959742869",
-  appId: "1:385959742869:web:742f21711fc5d16f1dba9c",
+  authDomain: "chatify-afab3.firebaseapp.com",
+  projectId: "chatify-afab3",
+  storageBucket: "chatify-afab3.appspot.com",
+  messagingSenderId: "446474111653",
+  appId: "1:446474111653:web:b686b51386c121a742db4c",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
