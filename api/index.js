bf5952f1f32a7ceb8c2f5a1290e2bd069db69d9f
im-amna <amna160304@gmail.com>
2025-03-06 02:36:54 +0500

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import mongoose from "mongoose";
-
+import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -16,6 +16,7 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
