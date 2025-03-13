b3565131b53be30b347a3df0219be81a79e2f152
im-amna <amna160304@gmail.com>
2025-03-14 02:07:05 +0500

    add

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -45,15 +45,16 @@ export const google = async (req, res, next) => {
         .json(rest);
     } else {
       const generatedPassword =
-        Math.random().toString().slice(-8) + Math.random().toString().slice(-8);
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-      const newUser = User({
+      const newUser = new User({
         username:
-          req.body.name.split("").join("").toLowerCase() +
-          Math.random().toString().slice(-4),
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
-        avator: req.body.photo,
+        avatar: req.body.photo,
       });
       await newUser.save();
       const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
@@ -63,6 +64,15 @@ export const google = async (req, res, next) => {
         .status(200)
         .json(rest);
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged out!");
   } catch (error) {
     next(error);
   }
