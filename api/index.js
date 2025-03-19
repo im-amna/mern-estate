a2bbf6d03149e999efd7dd020a4c4eaba4802274
Abdul Mannan <a.mannan0607@gmail.com>
2025-03-20 01:18:58 +0500

    modified:   api/index.js

--- api/index.js
+++ api/index.js
@@ -33,11 +33,11 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
 app.get("/test", (_, res) => {
-  res.send("Hello world!")
-})
+  res.send("Hello world!");
+});
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
