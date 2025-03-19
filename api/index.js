4651ec34e51c6c12713b21be9da63e7e8dfe1ae5
Abdul Mannan <a.mannan0607@gmail.com>
2025-03-19 22:56:10 +0500

    bug fix

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
+import path from "path";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
-import cookieParser from "cookie-parser";
-import path from "path";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 
 mongoose
@@ -34,6 +34,10 @@ app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("/", (_, res) => {
+  res.send("Hello world!")
+})
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
