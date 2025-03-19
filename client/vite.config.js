708ac2d9d2f58d91aa1013e09f4ea7d8d2636e08
Abdul Mannan <a.mannan0607@gmail.com>
2025-03-20 00:14:17 +0500

    fe deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://mern-estate-backend-lovat.vercel.app",
         changeOrigin: true,
         secure: false,
       },
