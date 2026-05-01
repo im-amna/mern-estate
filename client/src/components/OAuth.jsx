af7fbdc9a2cfc2d581fe0add1d73bc7e8a97d1dc
im-amna <amna74345@gmail.com>
2026-05-01 04:55:54 -0700

    Switch from signInWithPopup to signInWithRedirect

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,6 +1,7 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import { app } from "../firebase";
 import { signInSuccess } from "../redux/user/userSlice";
 import { backend_url } from "../server";
@@ -8,29 +9,40 @@ import { backend_url } from "../server";
 export default function OAuth() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    getRedirectResult(auth)
+      .then(async (result) => {
+        if (result) {
+          const res = await fetch(`${backend_url}/api/auth/google`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              name: result.user.displayName,
+              email: result.user.email,
+              photo: result.user.photoURL,
+            }),
+          });
+          const data = await res.json();
+          dispatch(signInSuccess(data));
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        console.log("redirect error", error);
+      });
+  }, []);
+
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
-      const auth = getAuth(app);
-      const result = await signInWithPopup(auth, provider);
-      const res = await fetch(`${backend_url}/api/auth/google`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: result.user.displayName,
-          email: result.user.email,
-          photo: result.user.photoURL,
-        }),
-      });
-      const data = await res.json();
-      dispatch(signInSuccess(data));
-      navigate("/");
+      await signInWithRedirect(auth, provider);
     } catch (error) {
       console.log("could not sign in with google", error);
     }
   };
+
   return (
     <button
       onClick={handleGoogleClick}
