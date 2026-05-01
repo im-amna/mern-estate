67e7d7a71087e6adc887a65f4ae6e9332f556806
im-amna <amna74345@gmail.com>
2026-05-01 04:42:50 -0700

    Update Firebase configuration for new project

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,14 @@
-// Import the functions you need from the SDKs you need
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "chatify-afab3.firebaseapp.com",
-  projectId: "chatify-afab3",
-  storageBucket: "chatify-afab3.appspot.com",
-  messagingSenderId: "446474111653",
-  appId: "1:446474111653:web:b686b51386c121a742db4c",
+  authDomain: "mern-estate-18ad0.firebaseapp.com",
+  projectId: "mern-estate-18ad0",
+  storageBucket: "mern-estate-18ad0.firebasestorage.app",
+  messagingSenderId: "385959742869",
+  appId: "1:385959742869:web:742f21711fc5d16f1dba9c"
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
